b1c38f000a5329c3e59902892e80da42a4abd4a7
GeorgiaDavid2006 <W0512995@campus.nscc.ca>
2025-11-26 11:08:16 -0400

    Timeline1 Complete

--- Assets/Scripts/TimelineTrigger.cs
+++ Assets/Scripts/TimelineTrigger.cs
@@ -3,25 +3,13 @@ using UnityEngine.Playables;
 
 public class TimelineTrigger : MonoBehaviour
 {
-    public PlayableDirector timeline1;
-    public GameObject player;
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public PlayableDirector timeline;
 
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
-            timeline1.Play();
+            timeline.Play();
         }
     }
 }
